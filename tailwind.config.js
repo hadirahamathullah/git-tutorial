0ddf3b6c16001d3664b12b21acaae6f969a981b3
hadirahamathullah <hadirahamathullah@gmail.com>
2022-05-16 17:21:31 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}", ],
+    theme: {
+
+        extend: {
+
+            fontFamily: {
+                exo: ['Exo', 'sans-serif'],
+
+            }
+
+
+        },
+    },
+    plugins: [],
+}
